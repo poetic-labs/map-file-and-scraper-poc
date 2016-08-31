88e6bde60f6073770ce0cef7e734cd793b7ab5b7
martinbee <martin@poeticsystems.com>
2016-08-31 15:40:46 -0500

    work on mapfile.

--- scraper.js
+++ scraper.js
@@ -1,10 +1,12 @@
+var path = require('path');
+var mapFilePath = path.resolve('map-file.json');
+var mapFile = require(mapFilePath);
 var url = 'http://app-ypo-v3.webflow.io/';
 var scraper = require('website-scraper');
 var fs = require('fs');
 
-// Downloads all the crawlable files of example.com.
-// The files are saved in the same structure as the structure of the website, by using the `bySiteStructure` filenameGenerator.
-// Links to other websites are filtered out by the urlFilter
+console.log(typeof mapFile);
+console.log(mapFile.URLS);
 
 // be very careful with this function
 var deleteFolderRecursive = function(path) {
@@ -25,7 +27,10 @@ var deleteFolderRecursive = function(path) {
 
 deleteFolderRecursive('./webflow');
 
-// basic scrape of one page
+// Downloads all the crawlable files of example.com.
+// The files are saved in the same structure as the structure of the website, by using the `bySiteStructure` filenameGenerator.
+// Links to other websites are filtered out by the urlFilter
+
 // scraper.scrape({
   // urls: [url],
   // urlFilter: function(url){
@@ -46,6 +51,9 @@ scraper.scrape({
     {url: url + '/users', filename: 'users.html'},
     {url: url + '/events', filename: 'events.html'},
   ],
+  urlFilter: function(url){
+      return url.indexOf(url) === 0;
+  },
   directory: './webflow',
   subdirectories: [
     {directory: 'img', extensions: ['.jpg', '.png', '.svg']},
